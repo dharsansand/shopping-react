6df3bcd87446d110b71a71e8242d132fa6dc9cfe
dharsansand <dharsansand@gmail.com>
2025-01-08 15:38:31 +0530

    change  logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header ( {cartItems}){
     return  <nav className="navbar row">
     <div className="col-12 col-md-3">
       <div className="navbar-brand">
-        <Link to="/"><img width="150px" src="/images/logo.png" /></Link>
+        <Link to="/"><img width="150px" src="/images/dharsan.png" /></Link>
 
 
       </div>
